77d431361145d7688cd4b0ca29a71bb6a86799e4
Diego Ochoa <diego.ochoa@ie-consulting.net>
2024-11-29 17:00:47 -0500

    Init project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-2xl text-blue-600 py-1">
+            Categorias ({{ categories.length }})
+            <button class="btn">add</button>
+        </h1>
+
+        <Category 
+          v-for="(category, idx) in categories"
+          :category="category"
+          @add="(category) => addChildCategory(category, idx)"
+        />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import Category from './components/Category.vue'
+
+const categories = ref([
+    { id: 1, name: 'cat1', child: []},
+    { id: 2, name: 'cat2', child: []},
+    { id: 3, name: 'cat3', child: []},
+])
+
+const addChildCategory = (category, idx) => {
+    categories.value[idx].child.push(category)
+}
+</script>
---
+++ src/components/Category.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div class="border border-blue-500 p-2">
+        {{ category.name }}: ({{ category.child.length }})
+        <button class="btn" @click="add"> Add </button>
+        <Category 
+            v-for="category in category.child" 
+            :category="category"
+            @add="addChildCategory"
+        />
+    </div>
+</template>
+
+<script setup>
+const { category } = defineProps(['category'])
+
+const emit = defineEmits(['add'])
+
+const add = () => {
+    const order = category.child.length + 1
+
+    emit('add', { name: 'child-'+ order, child: []} )
+}
+
+const addChildCategory = (category) => {
+    emit('add', category)
+}
+</script>
